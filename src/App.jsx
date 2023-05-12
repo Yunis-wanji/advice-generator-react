61634b876e20fcf0ac9c703a70df26e9a416c758
Yunis-wanji <kimaniwanjiru025@gmail.com>
2023-05-12 09:51:50 +0300

    setting up

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+const App =()=> {
+ 
+
+  return (
+    
+    <div className="flex  justify-center items-center h-screen bg-dark-blue font-Manrope font-semibold tracking-wide">
+      <article className="mx-3 my-6  pb-10 rounded-lg border-2 border-grey-1 text-center bg-grey-1 max-w-sm">
+                   <h4 className="text-[13px] text-green-1 py-5 "> hello</h4>
+      </article>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "index.html",
+    "./src/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {
+      colors:{
+        "dark-blue": "#1f2631",
+        "grey-1":"#313a49",
+        "green-1":"#58fcae",
+        "white-1":"#ccdfe6"
+      }
+    },
+  },
+  plugins: [],
+}
+
